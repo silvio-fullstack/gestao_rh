38e8828817eccb305f29c6f1ecd5f222e70da0bb
silvio_fullstack <sbalves@bol.com.br>
2020-11-13 20:18:50 -0300

    segundo commit

---
+++ apps/departamentos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=70, help_text='Nome do Departamento')
+    def __str__(self):
+        return self.nome
