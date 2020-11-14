c147d207a58982ed310a13fc4e226a023068fc0f
silvio_fullstack <sbalves@bol.com.br>
2020-11-14 01:09:22 -0300

    Criado alguns bancos de dados

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 from apps.departamentos.models import Departamento
 
 
+
 # Create your models here.
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Funcionario')
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     departamento = models.ManyToManyField(Departamento)
+    
     def __str__(self):
         return self.nome
