1ea36b588ba2011b5df7347fcc75c1f38301323d
silvio_fullstack <sbalves@bol.com.br>
2020-11-14 02:26:31 -0300

    Ajsute dos bancos de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -5,6 +5,8 @@ from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text='descrição do documento')
-    pertence = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
+    pertence = models.ForeignKey(
+        Funcionario, on_delete=models.SET_NULL, null=True, blank=True
+        )
     def __str__(self):
         return self.descricao
