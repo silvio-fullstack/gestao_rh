6ebec710a39f312b1e907b345cef350b917c6eed
silvio-fullstack <sbalves@bol.com.br>
2020-11-15 20:24:54 -0300

    CRUD de Empresas segunda parte

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Empresa
+from django.http import HttpResponse
+
 
 # Create your views here.
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
 
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('OK')
